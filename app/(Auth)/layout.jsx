4da1b9290921bae5d19fda13985fcf925dd2984a
vikalp-img <vikalp.img@gmail.com>
2026-04-24 10:11:30 +0530

    added newly code

--- app/(Auth)/layout.jsx
+++ app/(Auth)/layout.jsx
@@ -1,10 +1,14 @@
 export default function AuthLayout({ children }) {
   return (
-    <div className="flex min-h-screen">
-        <video src="background-video.mp4" autoPlay loop muted className="absolute w-full h-full object-cover">
-
-        </video>
-      {children}
-    </div>
+    <>
+      <div className="h-full overflow-hidden">
+        <video className="size-full object-cover max-md:hidden " src="https://nashatra-s3.s3.ap-south-1.amazonaws.com/login_bg1-2.mp4" autoPlay muted loop></video>
+        <div className="md:fixed top-0 left-0 size-full bg-primary-light md:bg-black/30 flex items-center justify-center">
+          <div className="h-dvh md:max-h-[92dvh] md:h-full w-full md:max-w-123 bg-primary-light md:rounded-2xl">
+            {children}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
